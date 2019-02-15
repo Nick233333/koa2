dc2419d469bf17476400f64ddd4d23f66de34043
nick <nick_php@163.com>
2019-02-15 17:19:56 +0800

    :art: add cli input port

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const config = require('./config/config');
 mongoose.connect(config.mongodb, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
 let figlet = require('figlet');
 const chalk = require('chalk');
+const inquirer = require('inquirer');
+
 
 marked.setOptions({
 	renderer: new marked.Renderer(),
@@ -58,16 +60,25 @@ app.use(flash());
 
 router(app);
 let server;
-server = app.listen(process.env.PORT, async () => {
-    await figlet('HELLO', (err, data) => {
-        if (err) {
-            return;
-        }
-        console.log(chalk.blue(data))
-        console.log(chalk.underline.yellow(`server is running at http://127.0.0.1:${process.env.PORT}`))
-    });
 
+inquirer.prompt({
+    type: 'input',
+    name: 'port',
+    message: chalk.red('Please input port:'),
+    default: process.env.PORT
+}).then(answers => {
+    process.env.PORT = answers.port;
+    server = app.listen(process.env.PORT, async () => {
+        await figlet('HELLO', (err, data) => {
+            if (err) {
+                return;
+            }
+            console.log(chalk.blue(data))
+            console.log(chalk.underline.yellow(`server is running at http://127.0.0.1:${process.env.PORT}`))
+        });
+    });
 });
+
 let onLineNum = 0;
 let io = require('socket.io')(server);
 io.on('connection', function (socket) {
